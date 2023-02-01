d2567992da3224ceb9b9c52b2d550a3e06a70f84
Pedro Oliveira <pedro86gugalegal@gmail.com>
2023-02-01 07:06:47 -0300

    Criando a classe ParkingSpotController e a classe ParkingSpotDto

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,5 @@
 package com.api.parkingcontrol.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
@@ -8,7 +7,10 @@ import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 @Service
 public class ParkingSpotService {
 
-	@Autowired
-	ParkingSpotRepository parkingSpotRepository;
+	final ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
 
 }
