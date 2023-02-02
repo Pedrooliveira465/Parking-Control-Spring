bc34ac08b4d1a627c5a2dd029cbd423716cab98d
Pedro Oliveira <pedro86gugalegal@gmail.com>
2023-02-02 09:14:36 -0300

    Criando o método get por id

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,8 @@ import com.api.parkingcontrol.services.ParkingSpotService;
 
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -53,6 +56,15 @@ public class ParkingSpotController {
 	@GetMapping
 	public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots() {
 		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id) {
+		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+		if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
 	}
 
 }
